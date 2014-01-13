fdc3f0e98c885a6894a11c41c92107229e1483a1
happen <zhanghaipeng404@gmail.com>
2014-01-14 00:35:20 +0800

    Update GoodsModel.class.php

--- CollegeLife/Home/Model/GoodsModel.class.php
+++ CollegeLife/Home/Model/GoodsModel.class.php
@@ -4,9 +4,9 @@ namespace Home\Model;
 use Think\Model\RelationModel;
 
 class GoodsModel extends RelationModel {
-	// 关联模型
+    // 关联模型
     protected $_link = array(
-		// 一个商品对应一个类别
+        // 一个商品对应一个类别
         'category' => array(
             'mapping_type' => BELONGS_TO,
             'class_name' => 'category',
