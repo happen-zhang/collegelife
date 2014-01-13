6e7061d74a1c9cda8c5c230a86cc1710e6e452d9
happen <zhanghaipeng404@gmail.com>
2014-01-14 00:36:33 +0800

    Update OrderModel.class.php

--- CollegeLife/Home/Model/OrderModel.class.php
+++ CollegeLife/Home/Model/OrderModel.class.php
@@ -4,13 +4,13 @@ namespace Home\Model;
 use Think\Model\RelationModel;
 
 class OrderModel extends RelationModel {
-	// 关联模型
-	protected $_link = array(
-		// 一个订单对应多个订单详情
+    // 关联模型
+    protected $_link = array(
+        // 一个订单对应多个订单详情
         'order_goods_ships' => array(
             'mapping_type' => HAS_MANY,
             'class_name' => 'OrderGoodsShip',
             'foreign_key' => 'order_id',
         ),
-	);
+    );
 }
